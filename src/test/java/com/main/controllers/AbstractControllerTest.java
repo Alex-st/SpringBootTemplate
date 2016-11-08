ef11d30aa498c4df57b5016e1a0ad8e604ee96b7
Alex <st_sanya@i.ua>
2016-11-08 17:29:20 +0200

    1.0.1 Controller tests done

--- src/test/java/com/main/controllers/AbstractControllerTest.java
+++ src/test/java/com/main/controllers/AbstractControllerTest.java
@@ -1,31 +1,26 @@
 package com.main.controllers;
 
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Created by Oleksandr on 10/19/2016.
  */
-@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MockServletContext.class)
-public class AbstractControllerTest {
+public abstract class AbstractControllerTest {
+
     protected MockMvc mockMvc;
 
-    @Autowired
-    protected WebApplicationContext webApplicationContext;
-
-    protected void initMockMvc() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
-
+    /*
+    There are two ways of initialization spring context for mock mvc:
+    1. MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
+    2. MockMvcBuilders.standaloneSetup(controller).build()
+    */
     protected void initBaseController(Object controller) {
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
